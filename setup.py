f74d9206ce619fdc178050e1a98ec499717cf5b5
Nick Fisher <github@nickdafish.com>
2015-05-27 18:17:20 -0400

    bones

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="influxdb_tools",
+    version="0.0.1",
+    package_dir={'': 'source'},
+    scripts=[
+        'scripts/influxdb_dumper',
+    ]
+)
---
+++ source/influxdb_tools/__init__.py
@@ -0,0 +1,2 @@
+def dumper():
+    print 'hello world'
