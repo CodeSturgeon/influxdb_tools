0123c19f10e35a614bc8a25b69ecd963cb764860
Nick Fisher <github@nickdafish.com>
2015-06-10 17:36:52 -0400

    working dumper

--- source/influxdb_tools/__init__.py
+++ source/influxdb_tools/__init__.py
@@ -2,26 +2,40 @@ import argparse
 from urllib import urlencode
 from urllib2 import urlopen
 from ConfigParser import SafeConfigParser
+import json
+import os
 
 
 DUMPER_DESC = 'Dump InfluxDB series to json files'
 LOADER_DESC = 'Load InfluxDB series from json files'
-INFLUXDB_URL_TEMPLATE = ''.join([
-        'http://{host}:{port}',
-        '/db/{dbname}/series?u={user}&p=pass',
-    ])
+INFLUXDB_URL_TEMPLATE = (
+        'http://{host}:{port}'
+        '/db/{dbname}/series?u={user}&p={pass}'
+    )
 
 
 def dumper():
+    # Setup cfg
     args = parse_args(DUMPER_DESC)
     cfg = load_cfg(args.config_file).defaults()
     cfg.update(args.__dict__)
-    print cfg
+    base_url = INFLUXDB_URL_TEMPLATE.format(**cfg)
+    # Get series list
+    series_url = base_url + '&' +  urlencode({'q': 'list series'})
+    resp = urlopen(series_url)
+    series_data = json.loads(resp.read())
+    series = [p[1] for p in series_data[0]['points']]
+    for s in series:
+        print s
+        s_url = base_url + '&' +  urlencode({'q': 'select * from "%s"'%s})
+        resp = urlopen(s_url)
+        s_path = os.path.join(cfg['data_dir'], s.replace('/', '_')+'.json')
+        open(s_path, 'w').write(resp.read())
 
 
 def parse_args(desc):
     parser = argparse.ArgumentParser(description=desc)
-    #parser.add_argument('dbname', help='Name of database to work with')
+    parser.add_argument('dbname', help='Name of database to work with')
     parser.add_argument('-d', '--data-dir', default='.',
             help='JSON files location')
     parser.add_argument('-c', '--config-file', default='./influxdb_tools.ini',
@@ -34,7 +48,7 @@ def load_cfg(cfg_path):
     cfg = SafeConfigParser(defaults={
             'user': 'root',
             'pass': 'root',
-            'addr': 'localhost',
+            'host': 'localhost',
             'port': '8086',
         })
     cfg.read(cfg_path)
